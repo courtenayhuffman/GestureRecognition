e99004c5b2072c436eb0000e58f13b9992cb676a
courtenayhuffman <courtenay.huffman@gmail.com>
2021-12-05 20:22:29 -0800

    add model predictions to demo

--- demo.py
+++ demo.py
@@ -1,4 +1,22 @@
 import cv2 as cv
+import numpy as np
+from keras.models import load_model
+
+CATEGORY_MAP = {
+    "call_me": 0,
+    "fingers_crossed": 1,
+    "okay": 2,
+    "paper": 3,
+    "peace": 4,
+    "rock": 5,
+    "rock_on": 6,
+    "scissor": 7,
+    "thumbs": 8,
+    "up": 9
+}
+
+def label_mapper(val):
+    return CATEGORY_MAP[val]
 
 # CAMERA INTERFACE
 def get_webcam():
@@ -14,3 +32,21 @@ def get_webcam():
             break
     cap.release()
     cv.destroyAllWindows
+
+# ML MODEL USE
+def predict_gesture(frame):
+    img = cv.imread(frame)
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    img = cv.resize(img, (225, 225))
+    model = load_model("gesture-model.h5")
+    prediction = model.predict(np.array([img]))
+    gesture_numeric = np.argmax(prediction[0]) #what is this even doing
+    gesture_name = label_mapper(gesture_numeric) 
+    print("Predicted Gesture: {}".format(gesture_name))
+
+# maybe??
+def detect_objects(img, net, outputLayers): # image, model, output layers
+	blob = cv.dnn.blobFromImage(img, scalefactor=0.00392, size=(320, 320), mean=(0, 0, 0), swapRB=True, crop=False)
+	net.setInput(blob)
+	outputs = net.forward(outputLayers)
+	return blob, outputs
