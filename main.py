76adef5a04f32646a08bb81278c4fd7e0432f95e
courtenayhuffman <courtenay.huffman@gmail.com>
2021-12-10 23:28:36 -0800

    deprecate main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 # COGS 300 Term Project
 # Gesture Recognition with ML
+# WARNING: THIS FILE IS DEPRECATED
 
 # things I need to import:
 #   dataset
 #   gulpIO
-import pandas as pd
-import numpy as np
-import cv2 as cv
-import os
-from keras_squeezenet import SqueezeNet
-from keras.optimizers import Adam
-from keras.utils import np_utils
-from keras.layers import Activation, Dropout, Convolution2D, GlobalAveragePooling2D
-from keras.models import Sequential
-import tensorflow as tf
+# import pandas as pd
+# import numpy as np
+# import cv2 as cv
+# import os
+# #from keras_squeezenet import SqueezeNet
+# from keras.optimizers import Adam
+# from keras.utils import np_utils
+# from keras.layers import Activation, Dropout, Convolution2D, GlobalAveragePooling2D
+# from keras.models import Sequential
+# import tensorflow as tf
 
